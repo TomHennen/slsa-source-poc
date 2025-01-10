933d31f5be9bb4a4194a5441f182150e83282d6a
Tom Hennen <TomHennen@users.noreply.github.com>
2025-01-10 09:19:24 -0500

    Didn't get this finished, submitting so I don't lose my work

--- actions/vsa_creator/determine_source_level_gh.sh
+++ actions/vsa_creator/determine_source_level_gh.sh
@@ -33,8 +33,32 @@ fi
 # We'll assume it meets this requirement if the branch prevents deletions
 # and force pushes.
 # TODO: Should other things be checked too?
-NO_DELETION=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="deletion") | any')
-NO_FORCE_PUSH=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="non_fast_forward") | any')
+DELETION_RULESET_ID=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="deletion").ruleset_id')
+NO_FORCE_PUSH_RULESET_ID=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="non_fast_forward").ruleset_id')
+
+# Do they both exist and have rulesets?
+if [ -z "${DELETION_RULESET_ID}" ] || [ -z "${NO_FORCE_PUSH_RULESET_ID}" ] ; then
+    echo "SLSA_SOURCE_LEVEL_1"
+    exit 0
+fi
+
+# TODO:
+# * Check if the needed rules are active.
+# * Check that they've been active for the last X days
+# * Use "updated_at" for the time rather than created
+#   * Folks can disable enforcement and change other important properties
+#     without changing
+# * Only run this script for 'push' events to 
+
+# Is the deletion rule active?
+DELETION_RULESET=$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/${REPO}/rulesets/${DELETION_RULESET_ID})
+
+ENFORCEMENT_STATUS=$(echo $DELETION_RULESET | jq '.enforcement')
+if [ "${ENFORCEMENT_STATUS}" = 'active' ]
+
 
 SOURCE_LEVEL="SLSA_SOURCE_LEVEL_1"
 
